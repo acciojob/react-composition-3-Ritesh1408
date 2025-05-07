432165dfe610cd5b836c4f267a0b24f28bc10ecd
RITESH ROUSHAN KUMAR <78962046+Ritesh1408@users.noreply.github.com>
2025-05-07 07:45:33 +0000

    Update the codebase

--- src/components/App.js
+++ src/components/App.js
@@ -1,13 +1,21 @@
 
 import React from "react";
+import Tooltip from "./Tooltip";
 import './../styles/App.css';
 
 const App = () => {
   return (
-    <div>
-        {/* Do not remove the main div */}
+    <div className="app-container">
+    
+      <Tooltip text="This is a tooltip">
+        <p className="tooltip-p">Hover over me</p>
+      </Tooltip>
+  
+      <Tooltip text="This is another Tooltip">
+        <p className="tooltip-text">Hover over me to see another tooltip</p>
+      </Tooltip>
     </div>
-  )
-}
+  );
+};
 
-export default App
+export default App;
